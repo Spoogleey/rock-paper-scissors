30135ea729b398536f08e6463a5b01447804cc43
Liam Green <122028106+Spoogleey@users.noreply.github.com>
2024-09-19 11:45:15 +0100

    Function for one round done
    Created the function which allows the user to play one round.

--- script.js
+++ script.js
@@ -1,22 +1,44 @@
-let humanScore = 0;
-let computerScore = 0;
-
+// Function to get a random choice for the computer
 function getComputerChoice() {
     let number = Math.floor(Math.random() * 3);
 
-    let choice = (number == 0) ? "Rock" :
-    (number == 1) ? "Paper" :
-    "Scissors";
+    let choice = (number == 0) ? "rock" :
+    (number == 1) ? "paper" :
+    "scissors";
 
     return choice;
 }
 
+// Function to get the choice from the player
 function getHumanChoice() {
     let player = "";
-    while(player == "" || player !== ("rock" || "paper" || "scissors")) {
+    while(player == "" || player !== "rock" && player !== "paper" && player !== "scissors") {
         player = prompt("Please choose rock, paper or scissors: ");
     }
     return player;
 }
 
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+    let choice = humanChoice.toLowerCase();
+
+    if(choice == computerChoice) {
+        let tie = console.log("It's a tie!");
+        return tie;
+    } else if((choice == "rock" && computerChoice == "scissors") || (choice == "paper" && computerChoice == "rock") || (choice == "scissors" && computerChoice == "paper")) {
+        let player = console.log(`You win! ${choice} beats ${computerChoice}`);
+        return player;
+    } else {
+        let computer = console.log(`You lose! ${computerChoice} beats ${choice}`);
+        return computer;
+    }
+}
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    let round = 0;
+
+    while(round < 5) {
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+}
